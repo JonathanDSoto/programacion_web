8e74fe054ebc4edd422c81065e0e71c0cd06d780
Jonathan Soto <>
2020-11-12 22:04:44 -0700

    hola mundo php

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php 
+	$name = "Jonathan";
+	$Name = "Juanito";
+	$lastname = "Soto";
+	$fullname = $name." ".$lastname;
+	//esto es un comentario de linea
+	/*esto es codigo
+	basura*/
+	#otro comentario de linea
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Site
+	</title>
+</head>
+<body>
+	<h1> 
+		Hola mi nombre es <?= $fullname; ?>
+	</h1>
+	
+</body>
+</html>
