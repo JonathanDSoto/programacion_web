1d88311060cc91268b841f61694fa9e105287e0b
Jonathan Soto <>
2020-11-20 22:23:15 -0700

    implementación de update delete session

--- app/categoryController.php
+++ app/categoryController.php
@@ -1,5 +1,8 @@
 <?php
 
+if (!isset($_SESSION)) {
+	session_start();
+}
 include "connectionController.php";
 
 if (isset($_POST['action'])) {
@@ -16,6 +19,21 @@ if (isset($_POST['action'])) {
 			$categoryController->store($name,$description,$status);
 
 		break; 
+		case 'update':
+
+			$name = strip_tags($_POST['name']);
+			$description = strip_tags($_POST['description']);
+			$status = strip_tags($_POST['status']);
+			$id = strip_tags($_POST['id']);
+
+			$categoryController->update($id,$name,$description,$status);
+		break;
+		case 'destroy':
+
+			$id = strip_tags($_POST['id']);
+
+			$categoryController->destroy($id);
+		break;
 	}
 
 }
@@ -56,18 +74,83 @@ class CategoryController
 				$prepared_query->bind_param('sss',$name,$description,$status);
 				if ($prepared_query->execute()) {
 					
+					$_SESSION['success'] = "el registro se ha guardado correctamente";
+
 					header("Location:". $_SERVER['HTTP_REFERER'] );
 
 				}else{
+
+					$_SESSION['error'] = 'verifique los datos envíados';
+
 					header("Location:". $_SERVER['HTTP_REFERER'] );
 				}
 
 			}else{
+
+				$_SESSION['error'] = 'verifique la información del formulario';
+
 				header("Location:". $_SERVER['HTTP_REFERER'] );
 			}
 
 		}else{
+
+			$_SESSION['error'] = 'verifique la conexión a la base de datos';
+
 			header("Location:". $_SERVER['HTTP_REFERER'] );
+		}
+	}
+
+	public function update($id,$name,$description,$status)
+	{
+		$conn = connect();
+		if ($conn->connect_error==false) {
+			
+			if($id !="" && $name!="" && $description!="" && $status!=""){
+
+				$query = "update categories set name = ?, description = ?, status = ? where id = ?";
+				$prepared_query = $conn->prepare($query);
+				$prepared_query->bind_param('sssi',$name,$description,$status,$id);
+				if ($prepared_query->execute()) {
+
+					header("Location:".$_SERVER['HTTP_REFERER']);
+				}else{
+
+					header("Location:".$_SERVER['HTTP_REFERER']);
+				}
+
+			}else{
+				header("Location:".$_SERVER['HTTP_REFERER']);
+			}
+
+		}else{
+			header("Location:".$_SERVER['HTTP_REFERER']);
+		}
+	}
+
+	public function destroy($id)
+	{
+		$conn = connect();
+		if ($conn->connect_error==false) {
+			
+			if ($id != "") {
+				
+				$query = "delete from categories where id = ?";
+				$prepared_query = $conn->prepare($query);
+				$prepared_query->bind_param('i',$id);
+				if ($prepared_query->execute()) {
+
+					header("Location:".$_SERVER['HTTP_REFERER']);
+				}else{
+					header("Location:".$_SERVER['HTTP_REFERER']);
+				}
+
+			}else{
+				header("Location:".$_SERVER['HTTP_REFERER']);
+			}
+
+
+		}else{
+			header("Location:".$_SERVER['HTTP_REFERER']);
 		}
 	}
 }
--- categories/index.php
+++ categories/index.php
@@ -18,6 +18,9 @@
 			border: 1px solid black;
 			border-collapse: collapse;
 		}
+		#updateForm{
+			display: none;
+		}
 	</style>
 </head>
 <body>
@@ -25,7 +28,32 @@
 
 		<h1>
 			Categories
-		</h1> 
+		</h1>
+
+		<?php 
+
+		if (isset($_SESSION) && isset($_SESSION['error'])) {
+
+			echo "<h3> Error: ".$_SESSION['error']."</h3>";
+			unset($_SESSION['error']);
+
+		}
+
+		?> 
+
+		<?php if (isset($_SESSION) && isset($_SESSION['error']) ): ?>
+		<h3>
+			Error: <?= $_SESSION['error'] ?>
+		</h3>
+		<?php unset($_SESSION['error']); ?>
+		<?php endif ?>
+
+		<?php if (isset($_SESSION) && isset($_SESSION['success']) ): ?>
+		<h3>
+			Correcto: <?= $_SESSION['success'] ?>
+		</h3>
+		<?php unset($_SESSION['success']); ?>
+		<?php endif ?>
 		
 		<table>
 			<thead>
@@ -37,6 +65,9 @@
 				</th>
 				<th>
 					Description
+				</th>
+				<th>
+					Actions
 				</th>
 			</thead>
 			<tbody>
@@ -54,13 +85,21 @@
 					<td>
 						<?= $category['description'] ?>
 					</td>
+					<td>
+						<button onclick="edit(<?= $category['id'] ?>,'<?= $category['name'] ?>','<?= $category['description'] ?>','<?= $category['status'] ?>')">
+							Edit category
+						</button>
+
+						<button onclick="remove(<?= $category['id'] ?>)" style="background-color: red;color:white;">
+							Delete category
+						</button>
+					</td>
 
 				</tr>
 					
-				<?php endforeach ?>
+				<?php endforeach  ?>
 				
-				<?php 
-
+				<?php   
 
 				// foreach ($categories as $category) {
 					
@@ -76,6 +115,15 @@
 				// 			".$category['description']."
 				// 		</td>
 
+				// 		<td>
+				// 			<button onclick='edit(".$category['id'].",\"".$category['name']."\",\"".$category['description']."\",\"".$category['status']."\")' >
+				// 				Edit category
+				// 			</button>
+
+				// 			<button onclick='delete(".$category['id'].")' > Delete category </button>
+
+				// 		</td>
+
 				// 	</tr>";
 
 				// }
@@ -86,7 +134,7 @@
 			</tbody>
 		</table>
 
-		<form action="../app/categoryController.php" method="POST">
+		<form id="storeForm" action="../app/categoryController.php" method="POST">
 			<fieldset>
 				
 				<legend>
@@ -120,6 +168,71 @@
 			</fieldset>
 		</form>
 
+		<form id="updateForm" action="../app/categoryController.php" method="POST">
+			<fieldset>
+				
+				<legend>
+					Edit Category
+				</legend>
+
+				<label>
+					Name
+				</label>
+				<input type="text" id="name" name="name" placeholder="terror" required=""> 
+				<br>
+
+				<label>
+					Description
+				</label>
+				<textarea placeholder="write here" id="description" name="description" rows="5" required=""></textarea>
+				<br>
+
+				<label>
+					Status
+				</label>
+				<select id="status" name="status">
+					<option> active </option>
+					<option> inactive </option>
+				</select>
+				<br>
+
+				<button type="submit" >Save Category</button>
+				<input type="hidden" name="action" value="update">
+				<input type="hidden" name="id" id="id">
+
+			</fieldset>
+		</form>
+
+		<form id="destroyForm" action="../app/categoryController.php" method="POST">
+
+			<input type="hidden" name="action" value="destroy">
+			<input type="hidden" name="id" id="id_destroy">
+
+		</form>
+
 	</div>
+	<script type="text/javascript">
+		function edit(id,name,description,status)
+		{	
+			document.getElementById('storeForm').style.display="none";
+			document.getElementById('updateForm').style.display="block";
+
+			document.getElementById('name').value = name;
+			document.getElementById('description').value = description;
+			document.getElementById('status').value = status;
+			document.getElementById('id').value = id;
+		}
+
+		function remove(id)
+		{
+			var confirm = prompt("Si quiere eliminar el registro, escriba :"+id);
+			if (confirm == id) {
+
+				document.getElementById('id_destroy').value = id;
+				document.getElementById('destroyForm').submit();
+
+			}
+		}
+	</script>
 </body>
 </html>
